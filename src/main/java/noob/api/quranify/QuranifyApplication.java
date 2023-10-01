803a9918d7088195d0c57cdbb149fcd8dcd90caa
abdelmoneim <abdelmoneimelshafei@proton.me>
2023-10-01 16:02:33 +0300

    first commit

---
+++ src/main/java/noob/api/quranify/QuranifyApplication.java
@@ -0,0 +1,13 @@
+package noob.api.quranify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuranifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuranifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noob/api/quranify/QuranifyApplicationTests.java
@@ -0,0 +1,13 @@
+package noob.api.quranify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuranifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
